47e076196ab63abf88820b250535d9c9a2046f05
阿卡琳 <hufan.akarin@Gmail.com>
2018-09-02 19:08:44 +0800

    tweak repo url

--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -62,7 +62,7 @@ class Preferences extends Component {
         <section>
           <div className="section-group">
             <div>{pref.i18n["preferences"]["github-repo"]}</div>
-            <a href="https://git.io/lrc-maker" target="_blank">
+            <a href="https://github.com/hufan-akari/lrc-maker" target="_blank">
               Github
             </a>
           </div>
