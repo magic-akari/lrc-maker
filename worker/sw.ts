4599f084b976e3c0faa64536b5c65622426a2bf1
阿卡琳 <hufan.akarin@Gmail.com>
2019-01-06 23:16:42 +0800

    bump veersion

--- worker/sw.ts
+++ worker/sw.ts
@@ -2,7 +2,7 @@ declare const self: ServiceWorkerGlobalScope;
 export {};
 
 const APP_NAME = "akari-lrc-maker";
-const VERSION = "5.0.0-alpha.20190105";
+const VERSION = "5.0.0-alpha.20190106";
 
 const supportDynamicImport = (() => {
     try {
