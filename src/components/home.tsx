7b17255b79fb10a9b04376d56cf6b4e95cb8f647
阿卡琳 <hufan.akarin@Gmail.com>
2019-01-09 22:12:39 +0800

    add home tip

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,3 +1,10 @@
 export const Home: React.FC = () => {
-    return <div>Home</div>;
+    return (
+        <div className="home">
+            <p className="home-tip-top-left">点击这里可以回到这个帮助页面</p>
+            <p className="home-tip-top-right">点击这里切换页面</p>
+            <p className="home-tip-bottom-left">这里可以加载音频，控制播放</p>
+            <p className="home-tip-bottom-right">这里可以调节播放速度</p>
+        </div>
+    );
 };
