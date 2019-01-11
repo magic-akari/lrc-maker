b99c864f8baa8789040fc2a4f9bf6cd9d25841a0
阿卡琳 <hufan.akarin@Gmail.com>
2019-01-11 21:58:31 +0800

    adjust home css

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,16 @@
 export const Home: React.FC = () => {
     return (
         <div className="home">
-            <p className="home-tip-top-left">点击这里可以回到这个帮助页面</p>
-            <p className="home-tip-top-right">点击这里切换页面</p>
-            <p className="home-tip-bottom-left">这里可以加载音频，控制播放</p>
-            <p className="home-tip-bottom-right">这里可以调节播放速度</p>
+            <p className="home-tip home-tip-top-left">
+                点击这里可以回到这个帮助页面
+            </p>
+            <p className="home-tip home-tip-top-right">点击这里切换页面</p>
+            <p className="home-tip home-tip-bottom-left">
+                这里可以加载音频，控制播放
+            </p>
+            <p className="home-tip home-tip-bottom-right">
+                这里可以调节播放速度
+            </p>
         </div>
     );
 };
