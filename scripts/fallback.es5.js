ada333fa552068eac5968fcb06cb0258cebf6e6e
magic-akari <hufan.akarin@gmail.com>
2019-04-12 20:59:29 +0800

    update: fallback.es5

--- scripts/fallback.es5.js
+++ scripts/fallback.es5.js
@@ -1,5 +1,6 @@
-if (typeof Proxy !== "function") {
-    if (confirm("ECMAScript 6 is not supported by this browser.\nRedirect to version 3.x of lrc-maker?")) {
-        location.href = "https://lrc-maker.github.io/3.x";
-    }
+if (
+    typeof Proxy !== "function" &&
+    confirm("ECMAScript 6 is not supported by this browser.\nRedirect to version 3.x of lrc-maker?")
+) {
+    location.href = "https://lrc-maker.github.io/3.x";
 }
