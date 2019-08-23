0951ed5ea8dd0b4011a5930d46c5050ee3be1c23
阿卡琳 <hufan.akarin@Gmail.com>
2019-08-23 17:10:44 +0800

    update: remove redundant code

--- src/utils/pubsub.ts
+++ src/utils/pubsub.ts
@@ -1,9 +1,9 @@
-export const createPubSub = <DT, ID = any>() => {
+export const createPubSub = <DT, ID = unknown>() => {
     type CB = (data: DT) => any;
     const bus = new Map<ID, CB>();
 
     const pub = (data: DT) => {
-        Array.from(bus.values()).forEach((cb: CB) => cb(data));
+        bus.forEach((cb: CB) => cb(data));
     };
     const sub = (id: ID, cb: CB) => {
         bus.set(id, cb);
