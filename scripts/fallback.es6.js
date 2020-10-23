6407b64c250dead046f3a4d879bb14b37600b3c6
magic-akari <akari.ccino@gmail.com>
2020-10-23 22:32:18 +0800

    fix: edge 16 error fallback

--- scripts/fallback.es6.js
+++ scripts/fallback.es6.js
@@ -1,15 +1,13 @@
 window.addEventListener("error", (ev) => {
-    if (
-        (ev.message.includes("SyntaxError") && ev.message.includes("import")) ||
-        (ev.message === "Syntax error" && ev.error.source && ev.error.source.includes("import"))
-    ) {
+    const { message: m, error: e } = ev;
+    if ((/SyntaxError/.test(m) && /import/.test(m)) || (/Syntax error/.test(e.message) && /import/.test(e.source))) {
         ev.preventDefault();
         console.log("dynamic module import is not implemented. fallback...");
         const es6 = document.querySelector(".index-es6");
-        const script = document.createElement("script");
-        script.src = es6.src;
-        script.integrity = es6.integrity;
-        script.crossOrigin = es6.crossOrigin;
-        document.head.appendChild(script);
+        const sc = document.createElement("script");
+        sc.src = es6.src;
+        sc.integrity = es6.integrity;
+        sc.crossOrigin = es6.crossOrigin;
+        document.head.appendChild(sc);
     }
 });
