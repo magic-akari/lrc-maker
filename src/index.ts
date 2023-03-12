419cb93b696d78cd636562968f9c2d0fb2438545
magic-akari <akari.ccino@gmail.com>
2023-03-12 17:22:09 +0800

    fix: css `:target`

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
 import { createElement } from "npm:react";
-import { createRoot } from "npm:react-dom/client";
+import { render } from "npm:react-dom";
 import { App } from "./components/app.js";
 
 if (!("scrollBehavior" in document.documentElement.style)) {
     import("./polyfill/smooth-scroll.js");
 }
 
-const container = document.querySelector(".app-container");
-const root = createRoot(container!);
+// Do not use createRoot
+// before https://github.com/facebook/react/issues/26374 fixed
+render(createElement(App), document.querySelector(".app-container"), () => {
+    document.querySelector(".page-loading")!.remove();
 
-root.render(createElement(App));
+    if (navigator.standalone || window.matchMedia("(display-mode: standalone)").matches) {
+        document.addEventListener("click", (ev) => {
+            const href = (ev.target as HTMLAnchorElement).getAttribute("href");
 
-if (navigator.standalone || window.matchMedia("(display-mode: standalone)").matches) {
-    document.addEventListener("click", (ev) => {
-        const href = (ev.target as HTMLAnchorElement).getAttribute("href");
+            if (href?.startsWith("#")) {
+                ev.preventDefault();
+                location.replace(href);
+            }
+        });
+    }
 
-        if (href?.startsWith("#")) {
-            ev.preventDefault();
-            location.replace(href);
-        }
+    window.addEventListener("dragover", (ev) => {
+        ev.preventDefault();
+        ev.dataTransfer!.dropEffect = "copy";
     });
-}
-
-window.addEventListener("dragover", (ev) => {
-    ev.preventDefault();
-    ev.dataTransfer!.dropEffect = "copy";
-});
-window.addEventListener("drop", (ev) => {
-    ev.preventDefault();
+    window.addEventListener("drop", (ev) => {
+        ev.preventDefault();
+    });
 });
